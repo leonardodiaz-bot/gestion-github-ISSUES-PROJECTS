3129f58a7ddc4400e6b30ae4234bb42a15004477
Carolina Cota <ninoska.cota@sistemas.edu.bo>
2026-09-08 03:54:40 -0400

    Corregir observaciones del issue #32

--- issue32_validar_semestre.py
+++ issue32_validar_semestre.py
@@ -1,8 +1,6 @@
-semestre = int(input("Ingrese semestre: "))
+semestre = int(input("Ingrese semestre (1-10): "))
 
 if 1 <= semestre <= 10:
     print("Semestre válido")
 else:
-    print("Semestre no válido")
-
-    
+    print("Semestre no válido. Solo se permiten valores del 1 al 10.")
